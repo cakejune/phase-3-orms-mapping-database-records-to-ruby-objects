f78a9337e688b932d14a7a5a80efecf316513594
cakejune <tyq444@gmail.com>
2022-11-28 14:35:57 -0500

    commit tree c468d6b49127ce50e76b7d6e131a74d89dee4759

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,15 @@
 class Song
-
   attr_accessor :name, :album, :id
 
   def initialize(name:, album:, id: nil)
     @id = id
     @name = name
     @album = album
+  end
+
+  def self.new_from_db(row)
+    # self.new is equivalent to Song.new
+    self.new(id: row[0], name: row[1], album: row[2])
   end
 
   def self.drop_table
@@ -49,4 +53,23 @@ class Song
     song.save
   end
 
+  def self.all
+    sql = <<-SQL
+    SELECT *
+    FROM songs
+  SQL
+
+    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
+  end
 end
